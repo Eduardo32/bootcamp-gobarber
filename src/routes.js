8c3db02f1c6bc8e6b515facc0aea85ecda80aba9
Eduardo <p.eduardo.32@gmail.com>
2019-10-25 00:29:26 -0300

    Criando agendamentos

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentContoller from './app/controllers/AppointmentContoller';
 
 // Middlewares
 import authMiddleware from './app/middlewares/auth';
@@ -21,6 +22,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentContoller.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
